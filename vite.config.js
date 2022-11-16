d184dca853498fd911f069f40d2931580b6a33fd
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-16 00:00:16 -0300

    deploy configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(),
+    chunkSplitPlugin({
+      strategy: 'default',
+      customSplitting:{
+        'react-vendor': ['react', 'react-dom', 'react-router-dom']
+      }
+    })
+  ],
 })
